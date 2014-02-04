96c1db8213b7e3d04d66e21f638ee088d11d798d
Gilles Bouthenot <gilles@mate-vdi>
2014-02-04 16:02:54 +0100

    Auth: création de l'utilisateur dans le bon profile, enregistre à chaque connexion nom, prenom et email NEED MIGRATION 15

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -123,10 +123,23 @@ respondExt(function($rq, $rs, $ap) {
 
             $user = User::findFirst(array("login"=>$login));
             if (!$user) {
+                // crée un nouvel utilisateur
+                // cherche le profil de base
+                $profile = Userprofile::findFirstOrThrows(array("is_superadmin"=>0, "is_admin"=>0, "is_author"=>0));
+
                 $user = User::create();
+                $user->userprofile_id = $profile->id;
                 $user->login = $login;
-                $res = $user->save();
+                $user->auth_type = "ldap";
             }
+
+            // met à jour utilisateur avec données de fastannu
+            $user->email      = $userinfo->mail;
+            $user->name_first = $userinfo->pren;
+            $user->name_last  = $userinfo->nom;
+            $user->last_at    = Gb_String::date_iso();
+            $res = $user->save();
+
 
             $_SESSION["auth"] = array("id"=>$user->id, "login"=>$login, "logoutUrl"=>$sessionCas->getLogoutUrl(), "email"=>$userinfo->mail, "firstname"=>$userinfo->pren, "lastname"=>$userinfo->nom, "displayname"=>$userinfo->disp);
         }
